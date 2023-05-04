515cae1891f9150a35600b765a7c740e7889dcf1
gabrielrportugal <gabrielrportugal@outlook.com>
2023-05-04 15:02:29 -0300

    creating and reading transactions

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 - Tsx (Typescript loader)
 - Knex (Query builder)
 - Sqlite (Database)
+- Zod (Field validations)
+
+## RF 
+
+- [x] User should be able to create a new transaction;
+- [x] User should be able to read the transactions balance;
+- [x] User should be able to list all the transactions;
+- [x] User should be able to get info from one transaction;
+
+## RN
+
+- [x] The transactions can be 2 types (credit/debit);
+- [ ] Should identify user from requests;
+- [ ] User should see only his transactions
---
+++ src/routes/transactions.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, response) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return response.status(201).send()
+  })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .where('id', id)
+      .first()
+      .select()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+}
